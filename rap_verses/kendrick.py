f05c9fde9c61030538c1ec8a04f04c7438aeec5f
aalaja <48320239+aalaja@users.noreply.github.com>
2021-03-10 13:51:01 -0500

    Madlibs Beginner Project Commit

---
+++ rap_verses/kendrick.py
@@ -0,0 +1,9 @@
+def madlib():
+    cash = input("Cash: ")
+    noun = input("Noun: ")
+    verb = input("Verb: ")
+
+    madlib = f"Girl, I can buy your ass the world with my {cash} \
+    Ooh, that {noun} good, won't you {verb} it on my taste bloods?"
+
+    print(madlib)
